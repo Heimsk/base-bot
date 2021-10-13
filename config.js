1ed81293419dc21aa4a08ea215a26564fdaa2a68
Heimsk <64988539+Heimsk@users.noreply.github.com>
2021-10-13 16:43:21 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mainColor: "#00ff00", // A cor das embeds e etc
-  devs: ["465599065007194113"], // Os ids das pessoas que poderão usar os comandos privados
+  devs: [""], // Os ids das pessoas que poderão usar os comandos privados
   emojis: {
     error: "", // O emoji das mensagens de erro.
     success: "" // O emoji das mensagens de sucesso.
